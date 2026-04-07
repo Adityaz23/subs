878d524d16136bcceb8fd6c890381116a2e5e0e9
Aditya Kumar Soni <soniadityakumar651@gmail.com>
2026-04-07 16:04:05 +0530

    feat: Tabs navigation has been setupped

--- src/app/(tabs)/subscriptions.tsx
+++ src/app/(tabs)/subscriptions.tsx
@@ -4,12 +4,12 @@ import {styled} from "nativewind";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView)
-const Settings = () => {
+const subscriptions = () => {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text >settings</Text>
+      <Text >subscriptions</Text>
     </SafeAreaView>
   )
 }
 
-export default Settings
+export default subscriptions
